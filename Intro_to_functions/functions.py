26ee0e39334b8b572337c8bb648b5e7081f9c9c9
David Soto <ds051128@gmail.com>
2019-12-30 14:50:39 -0600

    Put in more notes about functions on default paramters and arguments

--- Intro_to_functions/functions.py
+++ Intro_to_functions/functions.py
@@ -27,7 +27,7 @@ loading_screen()
 # Parameters are variables that you can pass into the function
 # when you call it.
 
-                   #Parameter#
+                    #Parameter#
 def greet_customer(special_item):
     print("Welcome!")
     print("Our special is " + special_item + ".")
@@ -41,3 +41,37 @@ greet_customer("peanut butter")
 
 # The value between the parantheses when we call the function
 # is known as an argument of the function call.
+
+# You can make your function take more than one paramater. You just need
+# to seperate your parameters with commas. 
+
+def multiply( a , b ):
+    print(a * b)
+
+multiply(10 , 20) # 200
+
+# in multiply a and b are called positional arguments because their 
+# assignments deend on their positions in the function call.
+
+def add_two_nums(a, b):
+    print(a + b)
+
+# we can also pass these arguments as keyword arguments, where
+# we explicitly refer to what each argument is assigned to the 
+# function call.
+
+add_two_nums(a = 2, b = 3)
+
+# we can also use default arguments in our function. If the function is
+# called without an argument for that parameter, it relies on the
+# default.
+
+def divide_two_nums(a, b = 2):
+    print(a / b)
+
+divide_two_nums(6) # Since we are only setting the argument for a, 
+# b will automatically be defined as 2.
+
+# Once you give an argument a default value, 
+# no arguments that follow can be used positionally.
+
