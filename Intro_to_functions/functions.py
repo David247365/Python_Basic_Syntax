e1a5cccf3b8d25e860445baa3bfffb3317a5face
David Soto <ds051128@gmail.com>
2019-12-30 00:10:22 -0600

    created function sing_song()

---
+++ Intro_to_functions/functions.py
@@ -0,0 +1,11 @@
+# In simple terms a function in python is a tool that you can
+# use over and over again to produce consistent output from 
+# different inputs.
+
+# Defining a function
+def sing_song():
+    print("Words fall, out of my mouth")
+    print("They don't make a sound")
+    print("and they drift to the ground")
+
+sing_song()
